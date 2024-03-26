5f9f5208219229cbb83e896e7b87df0a9082c6c3
qosimov007 <mirjalolq274@gmail.com>
2024-03-26 19:50:45 +0200

    Hero

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import './App.css'
+
+function App() {
+
+
+
+
+  return (
+    <>
+    
+      <div className="container">
+        <nav className="nav">
+          <div className="logo"> <img src="src/img/Logo.png" alt="Logo" /></div>
+          <div className="nav-btn"><img src="src/img/Group 59.png" alt="" /></div>
+        </nav>
+
+
+        <div className="hero">
+            <h1>Search Our Customer <br Database /></h1>
+           
+            <input type="text"  placeholder='Find your customer here...'   />
+            <p>Get your customers onboarded with our technolgy</p>
+            <button>Let’s go</button>
+        </div>
+
+
+
+
+
+      </div>
+    </>
+  )
+}
+<container></container>
+export default App
